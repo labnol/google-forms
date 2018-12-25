e011584d869ae96755edb72d2c3bfbeca2205357
Amit Agarwal <amit@labnol.org>
2018-12-25 14:10:45 +0530

    Google Forms

---
+++ README.md
@@ -0,0 +1,20 @@
+# Google Form Limiter
+
+Use Google Apps Script to easily schedule your Google Form. You can configure the form to automatically open and/or close at a certain date and time. Similarly, the script can be configured to turn off your form after a certain number of responses have been submitted.
+
+- Tutorial: [How to Limit Google Form Responses](https://www.labnol.org/internet/schedule-google-forms/20707/)
+- Video: https://youtu.be/fHY8uI2VFP4
+- Download: [Google Forms Add-on](https://chrome.google.com/webstore/detail/email-notifications-for-f/acknfdkglemcidajjmehljifccmflhkm)
+
+
+## Requirements
+
+- Google Apps Script
+- [Apps Script Starter Kit](https://github.com/labnol/apps-script-starter)
+
+
+## Google Apps Script Developer 👨🏼‍💻
+
+[Amit Agarwal](https://digitalinspiration.com/google-developer) is a web geek and author of [labnol.org](https://www.labnol.org/), a popular tech how-to website. He frequently uses [Google Apps Script](https://ctrlq.org/) to automate workflows and enhance productivity. 
+
+Reach him on Twitter [@labnol](https://twitter.com/labnol) or email amit@labnol.org
---
+++ limiter.js
@@ -0,0 +1,82 @@
+// Set the form’s open and close dates in lines #1 & #2 in  YYYY-MM-DD HH:MM format. If you would rather not have an open or close date, just set the corresponding value to blank.
+
+FORM_OPEN_DATE   =  "2019-01-01 08:00";
+FORM_CLOSE_DATE  =  "2019-12-25 23:30";
+
+// Set the RESPONSE_COUNT equal to the total number of entries that you would like to receive after which the form is closed automatically. If you would not like to set a limit, set this value to blank.
+
+RESPONSE_COUNT   =  "100";
+
+/* Web tutorial: https://labnol.org/?p=20707  */
+
+/* Initialize the form, setup time based triggers */
+function Initialize() {
+  
+  deleteTriggers_();
+  
+  if ((FORM_OPEN_DATE !== "") && 
+      ((new Date()).getTime() < parseDate_(FORM_OPEN_DATE).getTime())) { 
+    closeForm();
+    ScriptApp.newTrigger("openForm")
+    .timeBased()
+    .at(parseDate_(FORM_OPEN_DATE))
+    .create();
+  }
+  
+  if (FORM_CLOSE_DATE !== "") { 
+    ScriptApp.newTrigger("closeForm")
+    .timeBased()
+    .at(parseDate_(FORM_CLOSE_DATE))
+    .create(); 
+  }
+  
+  if (RESPONSE_COUNT !== "") { 
+    ScriptApp.newTrigger("checkLimit")
+    .forForm(FormApp.getActiveForm())
+    .onFormSubmit()
+    .create();
+  }
+  
+}
+
+/* Delete all existing Script Triggers */
+function deleteTriggers_() {  
+  var triggers = ScriptApp.getProjectTriggers();  
+  for (var i in triggers) {
+    ScriptApp.deleteTrigger(triggers[i]);
+  }
+}
+
+/* Send a mail to the form owner when the form status changes */
+function informUser_(subject) {
+  var formURL = FormApp.getActiveForm().getPublishedUrl();
+  MailApp.sendEmail(Session.getActiveUser().getEmail(), subject, formURL);  
+}
+
+/* Allow Google Form to Accept Responses */
+function openForm() {
+  var form = FormApp.getActiveForm();
+  form.setAcceptingResponses(true);
+  informUser_("Your Google Form is now accepting responses");
+}
+
+/* Close the Google Form, Stop Accepting Reponses */
+function closeForm() {  
+  var form = FormApp.getActiveForm();
+  form.setAcceptingResponses(false);
+  deleteTriggers_();
+  informUser_("Your Google Form is no longer accepting responses");
+}
+
+/* If Total # of Form Responses >= Limit, Close Form */
+function checkLimit() {
+  if (FormApp.getActiveForm().getResponses().length >= RESPONSE_COUNT ) {
+    closeForm();
+  }  
+}
+
+/* Parse the Date for creating Time-Based Triggers */
+function parseDate_(d) {
+  return new Date(d.substr(0,4), d.substr(5,2)-1, 
+                  d.substr(8,2), d.substr(11,2), d.substr(14,2));
+}
